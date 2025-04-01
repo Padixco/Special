68b1aee5275cea147b6a7b5533db4601061310d7
Padixco <dennispedro006@gmail.com>
2025-04-01 22:28:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Countdown Timer (Set a special date)
+const countdownDate = new Date("2025-05-01T00:00:00").getTime();
+
+const countdownTimer = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown-timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
